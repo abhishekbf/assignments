b7ef1105d497a9c9f513662542e677379592aa5a
abhishekbf <145325719+abhishekbf@users.noreply.github.com>
2024-11-24 17:20:11 +0530

    Update findLargestElement.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,22 @@
 */
 
 function findLargestElement(numbers) {
+    let largest = arr[0];
+    for (let i = 1;i<=arr.lenght;i++){
+        if(arr[i]>largest)
+        {
+            largest = arr[1];
+            return largest;
+        } }
+    const number[1,6,4,5,6,9];
+    console.log(findLargestElement(number));
+            
+
+
+
+
+    
+    
     
 }
 
